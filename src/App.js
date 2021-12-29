61618ae354b19549d11c9ffa61a2ffe8cc6e9073
ImSrishti <shristi998@gmail.com>
2021-12-29 18:21:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2 className='heading'>Australia’s most secure & trusted garage door brand</h2>
+        <p className='p1'>We know that behind your garage door are the things that matter to you most. The things that make your house a home. Most garage doors just close but a B&D garage door can be locked securely shut.</p>
+        <div style={{marginBottom:"50px"}}>
+        <img className='icons' src="https://cdn-3.convertexperiments.com/uf/1002628/10025029/1605083565guarantee.png" alt="" />
+        <span className='pbold'>10 Years Warranty</span>
+        <span className='p2'>All our garage doors, components, and accessories - other than batteries, bulbs, or fuses - include a one to ten-year warranty. Safe as can B&D</span>
+        </div>
+        <div style={{marginBottom:"50px"}}>
+        <img className='icons' src="http://cdn-3.convertexperiments.com/uf/1002628/10025029/1605083627sixty.png" alt="" />
+        <span className='pbold'>10 Years Warranty</span>
+        <span className='p2'>All our garage doors, components, and accessories - other than batteries, bulbs, or fuses - include a one to ten-year warranty. Safe as can B&D</span>
+        </div>
+        <div style={{marginBottom:"50px"}}>
+        <img className='icons' src="http://cdn-3.convertexperiments.com/uf/1002628/10025029/1605083662australia.png" alt="" />
+        <span className='pbold'>10 Years Warranty</span>
+        <span className='p2'>All our garage doors, components, and accessories - other than batteries, bulbs, or fuses - include a one to ten-year warranty. Safe as can B&D</span>
+        </div>
+
+        <button className='button'>Our range of garage doors</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
